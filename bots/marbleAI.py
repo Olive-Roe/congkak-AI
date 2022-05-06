179a8fcfc4f53ef771387b6f800163e86f8054ae
Olive-Roe <14oloh@kl.his.edu.my>
2022-05-06 19:23:07 +0800

    Start marbleAI

--- bots/marbleAI.py
+++ bots/marbleAI.py
@@ -1,2 +1,10 @@
 '''An unfinished AI that will use a minimax algorithm, limiting the number of
 best moves at each depth to run faster.'''
+
+import congkak
+
+
+def play(turn, board, p1pit, p2pit, data):
+    bestEvaluation = float("-inf") if turn == 1 else float("inf")
+    moves = congkak.getLegalMoves(turn, board)
+    return "bestMove", "evaluation"
