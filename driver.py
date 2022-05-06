c6dabd4d159800863ec3e02c1d821ce4eb65f3dd
Olive-Roe <14oloh@kl.his.edu.my>
2022-05-06 18:49:34 +0800

    Add tournament functionality
    Now supports tournaments of different AIs, logging each game and outputting the results in a CSV file.

--- driver.py
+++ driver.py
@@ -27,14 +27,14 @@ def getMove(turn, board, name="player",  p1pit="", p2pit="", data=""):
         return move
 
 
-def playGame(replayFile="", player1="simple", player2="simple", replayName=""):
+def playGame(replayFile="", player1="simple", player2="simple", replayName="", pathToReplay="replays/"):
     moveList = []
     turn = 1
     board = setupBoard()
     p1pit, p2pit = 0, 0
     displayBoard(board, p1pit, p2pit, turn)
     if replayFile != "":
-        with open(f"replays/{replayFile}.txt", "r") as f:
+        with open(f"{pathToReplay}{replayFile}.txt", "r") as f:
             replay = f.readlines()[1].split(" ")
         for move in replay:
             if turn == 1 and sum(board[:BOARD_SIZE//2]) == 0 or turn == 2 and sum(board[BOARD_SIZE//2:]) == 0:
@@ -70,23 +70,23 @@ def playGame(replayFile="", player1="simple", player2="simple", replayName=""):
             continue
         turn = 2 if turn == 1 else 1  # change turns
         displayBoard(board, p1pit, p2pit, turn)
-
-    win = "No one" if p1pit == p2pit else "Player 1" if p1pit > p2pit else "Player 2"
+    win, winVal = ("No one", 0.5) if p1pit == p2pit else (
+        "Player 1", 1.0) if p1pit > p2pit else ("Player 2", 0)
     print(f"{win} wins.")
     if replayFile == "":
         if replayName == "":
             print(moveList)
             while True:
                 replayName = input("What would you like to call this replay? ")
-                if not(exists(f"replays/{replayName}.txt")):
+                if not(exists(f"{pathToReplay}{replayName}.txt")):
                     break
                 else:
                     print("This file name is already taken.")
-        with open(f"replays/{replayName}.txt", "w") as f:
-            f.write(time.strftime("%d/%m/%Y")+"\n")
+        with open(f"{pathToReplay}{replayName}.txt", "w") as f:
+            f.write(f'{time.strftime("%d/%m/%Y")} {float(winVal)}-{1.0-winVal}\n')
             f.write(" ".join(moveList))
-    return win
+    return winVal
 
 
 if __name__ == "__main__":
-    playGame()
+    print(playGame())
--- tournament_driver.py
+++ tournament_driver.py
@@ -1,6 +1,85 @@
 import driver
+import csv
+from pathlib import Path
+
 
 PLAYERS = ["random", "simple"]
 
-driver.playGame(replayFile="", player1="player", player2="simple",
-                replayName="player_vs_simple_5")
+
+def isFloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+# from https://stackoverflow.com/questions/11033590/change-specific-value-in-csv-file-via-python
+
+
+def editValue(columns, rows, values, file="tournament/score.csv", add=False):
+    r = csv.reader(open(file))
+    lines = list(r)
+    for i in range(len(columns)):
+        column, row, value = columns[i], rows[i], values[i]
+        if add:
+            assert type(value) in [int, float]
+            if isFloat(lines[column][row]):
+                lines[column][row] = str(float(lines[column][row]) + value)
+            else:  # if current item is not a number
+                lines[column][row] = value
+        else:
+            lines[column][row] = value
+    writer = csv.writer(open(file, 'w'))
+    writer.writerows(lines)
+
+
+def createEmptyFile(filename, sidelength):
+    with open(filename, "w") as f:
+        f.writelines(f'{","*sidelength}\n'*sidelength)
+
+# from https://stackoverflow.com/a/56151260
+
+
+def emptyDir(dirpath):
+    [f.unlink() for f in Path(dirpath).glob("*") if f.is_file()]
+
+
+def logGame(name, result, file="tournament/gamelog.txt"):
+    with open(file, "a") as f:
+        f.write(f"{name} {result}\n")
+
+def resetGameLog(file="tournament/gamelog.txt"):
+    with open(file, "w") as f:
+        pass
+
+def playTournament(players, rounds, filename="tournament/score.csv", replayFolder="tournament/games/", gamelog="tournament/gamelog.txt"):
+    resetGameLog(file=gamelog)
+    emptyDir(replayFolder)
+    # create file
+    createEmptyFile(filename, 10)
+    playerIndices = [i+1 for i in range(len(players))]
+    # from https://www.geeksforgeeks.org/python-dictionary-comprehension/
+    playerDict = dict(zip(players, playerIndices))
+    # writes vertical players
+    editValue([0]*len(players), playerIndices, players, file=filename)
+    # writes horizontal players
+    editValue(playerIndices, [0]*len(players), players, file=filename)
+    # writes / diagonally
+    # (used) (not used, as players will play against themselves)
+    editValue(playerIndices, playerIndices, ["/"]*len(players), file=filename)
+    for roundNum in range(rounds):
+        for player1 in players:
+            for player2 in players:
+                if player2 != player1:
+                    result = driver.playGame(player1=player1, player2=player2,
+                                             replayName=f"{player1}_vs_{player2}_{roundNum+1}", pathToReplay=replayFolder)
+                    logGame(f"{player1}_vs_{player2}_{roundNum+1}",
+                            f"{float(result)}-{1.0-result}", file=gamelog)
+                    editValue([playerDict[player1]], [playerDict[player2]],
+                              [float(result)], add=True, file=filename)
+                    editValue([playerDict[player2]], [playerDict[player1]],
+                              [1.0-result], add=True, file=filename)
+
+
+if __name__ == "__main__":
+    playTournament(PLAYERS, 10)
