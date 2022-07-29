2697d51b61f0bb27f95c44731fc36dc0ef32fad4
Olive-Roe <14oloh@kl.his.edu.my>
2022-07-29 23:34:19 +0800

    Train zeroAI to 57% win rate

--- zeroAI_driver.py
+++ zeroAI_driver.py
@@ -2,13 +2,9 @@ import driver
 import tournament_driver
 from time import time
 
-s = time()
-tournament_driver.playTournament(["zero", "simple"], 500)
-e = time()
-print(e-s)
 
-driver.playGame(replayFile="zero_vs_simple_5",
-                pathToReplay="tournament/games/")
+# driver.playGame(replayFile="zero_vs_simple_5",
+#                 pathToReplay="tournament/games/")
 
 
 def stamp_weights(name):
@@ -18,6 +14,24 @@ def stamp_weights(name):
         f.write(contents)
 
 
-stamp_weights("day 1")
+def get_win_percent(games):
+    multiplier = 100/games
+    with open("tournament/score.csv") as f:
+        lines = f.readlines()
+    number = lines[1].split(",")[2]
+    return int(float(number)*multiplier)
+
+
+def autolog(games=1000):
+    percent = get_win_percent(games)
+    with open("bots/zeroAI_data/automatic_log.txt", "a") as f:
+        f.write(f"\n{percent}%")
+
+
+for _ in range(1):
+    tournament_driver.playTournament(["zero", "simple"], 500)
+    stamp_weights("night 1")
+    autolog(1000)
+
 
 # driver.playGame(player1="player", player2="zero")
